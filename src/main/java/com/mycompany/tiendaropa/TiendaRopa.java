fc78dc413f1cb32c99a8d2da07494588917f5367
Jonnathan23 <114946886+Jonnathan23@users.noreply.github.com>
2024-01-10 16:54:55 -0500

    Inicio

---
+++ src/main/java/com/mycompany/tiendaropa/TiendaRopa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tiendaropa;
+
+/**
+ *
+ * @author User
+ */
+public class TiendaRopa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
